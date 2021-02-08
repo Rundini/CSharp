c24a1e4517f8033d91f9bc478ccaf990186c6bb1
Rundini <71119400+Rundini@users.noreply.github.com>
2021-02-08 09:34:49 +0100

    Add files via upload
    Lektion 01

---
+++ Lek 01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    // Opgave 4:
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            Console.WriteLine("Hello World!");
+
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+            aFriend = "Maira";
+            int alder = 20;
+            Console.WriteLine(aFriend);
+            Console.WriteLine("Hello " + aFriend);
+            Console.WriteLine($"Hello {aFriend}, she is {alder} years old");
+
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+            string greeting = "      Hello World!       ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+            Console.WriteLine(songLyrics.StartsWith("You"));
+            Console.WriteLine(songLyrics.EndsWith("hello"));
+            Console.WriteLine(songLyrics.EndsWith("hello."));
+
+
+            // Opgave 5:
+            double m1 = Max(28.5, 17.2);
+            double m2 = Max(4.0, 10.8, 34.25, 2.0, 23.6);
+
+            Console.WriteLine(m1 + " " + m2);
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+
+
+        // Opgave 5:
+        public static double Max(params double[]list)
+        {
+            double max = double.MinValue;
+
+            foreach(double tal in list)
+            {
+                if(tal > max)
+                {
+                    max = tal;
+                }
+            }
+
+            return max;
+        }
+
+    }
+}
---
+++ Lek 01/Lommeregner/Lommeregner/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lommeregner
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            Console.WriteLine("Konsol Lommeregner i C#\r");
+            Console.WriteLine("-----------------------\n");
+
+            while (!endApp)
+            {
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+                Console.Write("Indtast det først tal, og tryk ENTER: ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("Dette er ikke et gyldigt input! Indtast et tal! ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                Console.Write("Indtast det andet tal, og tryk ENTER: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("Dette er ikke et gyldigt input! Indtast et tal!");
+                    numInput2 = Console.ReadLine();
+                }
+
+                Console.WriteLine("\nVælg en regnefunktion:");
+                Console.WriteLine("\t+ - Læg sammen");
+                Console.WriteLine("\t- - Træk fra");
+                Console.WriteLine("\t* - Gange");
+                Console.WriteLine("\t/ - Dividér");
+                Console.Write("Dit valg: ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("\nDenne operation vil give en fejl!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nDit resultat er: {result}\n");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Der opstod en fejl under udregningen: " + e.Message);
+                }
+
+                Console.WriteLine("-----------------------\n");
+
+                Console.Write("Tast 'x' og ENTER for at afslutte, eller en vilkårlig tast og ENTER for at fortsætte!");
+                if (Console.ReadLine() == "x")
+                {
+                    endApp = true;
+                }
+
+                Console.WriteLine("\n");
+
+            }
+            return;
+        }
+    }
+
+}
